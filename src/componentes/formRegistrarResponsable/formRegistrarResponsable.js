3839ab46ed7e8d306b974fe4add9e29e53abec47
unknown <sebastianwilches2@gmail.com>
2022-12-14 18:12:22 -0500

    FormRegistrarResponsable - Consume los servicios del backend

--- src/componentes/formRegistrarResponsable/formRegistrarResponsable.js
+++ src/componentes/formRegistrarResponsable/formRegistrarResponsable.js
@@ -4,8 +4,8 @@ import "react-widgets/styles.css";
 
 import "./formRegistrarResponsable.css";
 
-export default function FormRegistrarResponsable() {
-  const [apartamento, setApartamento] = useState("# Apartamento");
+export default function FormRegistrarResponsable({ apartamentos }) {
+  const [AptoDropdown, setAptoDropdown] = useState("# Apartamento");
   const [persona, setPersona] = useState({
     k_numero: "",
     tipo: "",
@@ -41,22 +41,20 @@ export default function FormRegistrarResponsable() {
     let responsableCompleto;
     persona.k_numero = Number(persona.k_numero);
     persona.o_telefono = Number(persona.o_telefono);
-    if(responsable.es_propietario == 1){
-      responsable.es_propietario = true;  
-    }else{
+    if (responsable.es_propietario == 1) {
+      responsable.es_propietario = true;
+    } else {
       responsable.es_propietario = false;
     }
     responsable.es_propietario = true;
     responsable.o_tel_residencia = Number(responsable.o_tel_residencia);
     responsable.o_tel_trabajo = Number(responsable.o_tel_trabajo);
-    responsableCompleto = { persona, responsable, apartamento };
+    responsableCompleto = { persona, responsable, apartamento:Number(AptoDropdown) };
     console.log(responsableCompleto);
     sendResponsable(responsableCompleto);
   };
 
   const sendResponsable = async (object) => {
-    console.log("AQUI ESTA EL OBJETOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-    console.log(object);
     const response = await fetch(
       "http://localhost:8081/persona/save_persona_responsable",
       {
@@ -79,9 +77,11 @@ export default function FormRegistrarResponsable() {
       <h2>Registrar responsable de un apartamento</h2>
       <p>Apartamento: </p>
       <DropdownList
-        value={apartamento}
-        onChange={(nextValue) => setApartamento(nextValue)}
-        data={[101, 201, 103]}
+        dataKey="k_APARTAMENTO"
+        textField="k_APARTAMENTO"
+        value={AptoDropdown}
+        onChange={(nextValue) => setAptoDropdown(nextValue.k_APARTAMENTO)}
+        data={apartamentos}
       />
       <form className="formLogin" onSubmit={handleSendResponsable}>
         <p>Numero de Documento: </p>
--- src/vistas/responsable/Responsable.js
+++ src/vistas/responsable/Responsable.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 import FormRegistrarResponsable from '../../componentes/formRegistrarResponsable/formRegistrarResponsable'
 
 export default function Responsable() {
+    const [apartamentos, setApartamentos] = useState([]);
+
+    useEffect(() => {
+        GETApartamento();
+    }, [])
+
+    const GETApartamento = async () => {
+        const response = await fetch('http://localhost:8081/apartamento/findAll');
+        const data = await response.json();
+        
+        setApartamentos(data);
+    }
+
+
     return (
         <div>
-            <FormRegistrarResponsable/>
+            <FormRegistrarResponsable apartamentos={apartamentos}/>
         </div>
     )
 }
